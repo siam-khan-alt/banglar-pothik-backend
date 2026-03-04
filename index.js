3b77b9c1f08f1d69d5ea1f0cf14cc47535e817a3
siam-khan-alt <nssiam99@gmail.com>
2026-03-04 14:29:56 +0600

    [FEATURE]:initialize banglar-prothik-backend with express, mongodb, and dotenv

---
+++ index.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+app.use(cors())
+app.use(express.json())
+const port = process.env.PORT || 5000;
+
+const uri =`${process.env.MONGODB_URI}`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    app.get('/', (req, res) => {
+  res.send('Hello banglar pothik!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
