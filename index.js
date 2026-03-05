a6c9169de0f11842847b699b10a296b75a620db5
siam-khan-alt <nssiam99@gmail.com>
2026-03-05 10:01:13 +0600

    [FEATURE]:add user collection and error solve for vercel deploy

--- index.js
+++ index.js
@@ -4,11 +4,7 @@ const cors = require("cors");
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
-app.use(cors({
-  origin: "https://banglar-pothik.vercel.app", 
-  methods: ["GET", "POST", "PATCH", "DELETE", "PUT"],
-  credentials: true
-}))
+app.use(cors())
 app.use(express.json())
 const port = process.env.PORT || 5000;
 
@@ -23,13 +19,14 @@ const client = new MongoClient(uri, {
 });
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
 
     const db = client.db("banglar_pothik");
     const divisionscollection = db.collection("divisions")
     const districtsCollection = db.collection("districts");
     const upazilasCollection = db.collection("upazilas");
     const unionsCollection = db.collection("unions");
+    const usersCollection = db.collection("users");
 
 
     await client.db("admin").command({ ping: 1 });
@@ -58,11 +55,31 @@ app.get('/unions',  async (req, res) => {
 });
 
 
+app.post('/users', async (req, res) => {
+  const user = req.body;
+  
+  const query = { email: user.email };
+  const existingUser = await usersCollection.findOne(query);
+
+  if (existingUser) {
+    return res.send({ message: 'User already exists', insertedId: null });
+  }
+
+  const result = await usersCollection.insertOne(user);
+  res.send(result);
+});
+
+
+app.get('/users', async (req, res) => {
+  const result = await usersCollection.find().toArray();
+  res.send(result);
+});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
   } finally {
     // await client.close();
   }
 }
 run().catch(console.dir);
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
